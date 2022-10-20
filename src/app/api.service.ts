cbf7b56e705d203a4738f182ab2b1b76149a1ca4
SANDHRAMARIYA <sandhramariya09@gmail.com>
2022-10-20 12:41:18 +0530

    serce

---
+++ src/app/api.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http:HttpClient) { }
+
+  viewstudent=()=>{
+    return this.http.get("http://localhost:8080/view")
+  }
+
+  addstudent=(data:any)=>{
+    return this.http.post("http://localhost:8080/studentEntry",data)
+  }
+
+  deletestudent=(data:any)=>{
+    return this.http.post("http://localhost:8080/deletestudent",data)
+  }
+}
